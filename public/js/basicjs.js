af836474e82a8951648458fce0670e640000211b
rahulraj16mar <31818361+rahulraj16mar@users.noreply.github.com>
2018-09-06 21:52:30 +0530

    modified

--- public/js/basicjs.js
+++ public/js/basicjs.js
@@ -5,39 +5,3 @@ $(document).ready(function() {
     $('.navbar-content').toggleClass('navbar-content-slide');
   });
 })
-
-var ajax = {
-    isSubmiting: false,
-    send: function() {
-
-      if(ajax.isSubmiting == false) {
-        ajax.isSubmiting = true;
-
-        var chatEmailid = $("input[name=chatEmailid]").val();
-        var chatPhone = $("input[name=chatPhone]").val();
-        var chatMessage = $("textarea[name=chatMessage]").val();
-
-        if(chatEmailid == "" || chatPhone == "" || chatMessage == "")
-          alert("Form not complete");
-        else {
-       ajax.SetText("Sending...");
-          $.post("../chat/chat.php", {
-            chatEmailid: chatEmailid, chatPhone: chatPhone, chatMessage: chatMessage
-          }, function(data) {
-            if(data == "true") {
-              ajax.SetText("Sent!");
-            } else {
-              ajax.SetText("Send mail");
-              alert(data);
-            }
-
-            ajax.isSubmiting = false;
-          });
-        }
-      }
-      else alert("You can only send 1 email at a time");
-    },
-    SetText: function(text) {
-      $("input[type=button]").val(text);
-    }
-  }
